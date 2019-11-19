ac68df0d3ad5991a41630e500758d96d40f26e36
Kobe McKee <kobe.mckee@gmail.com>
2019-11-19 16:53:58 -0700

    Testing config

--- TreasureHunt/TreasureHunt/APIController.swift
+++ TreasureHunt/TreasureHunt/APIController.swift
@@ -13,7 +13,48 @@ class APIController {
     
     
     
-
+    
+    
+//    func initPlayer(completion: @escaping (Room?, Error?) -> Void) {
+//
+//        let requestURL = baseURL
+//            .appendingPathComponent("init")
+//            .appendingPathExtension("json")
+//
+//        var request = URLRequest(url: requestURL)
+//        request.httpMethod = "GET"
+//
+//        request.setValue("Token \(apiKey)", forHTTPHeaderField: "Authorization")
+//
+//        URLSession.shared.dataTask(with: request) { (data, response, error) in
+//
+//            if let error = error {
+//                NSLog("Error initializing player: \(error)")
+//                completion(nil, error)
+//                return
+//            }
+//
+//            guard let data = data else {
+//                NSLog("No data returned")
+//                completion(nil, NSError(domain: "No data returned", code: 001, userInfo: [NSLocalizedDescriptionKey : "No data returned from URLSession Data Task"]))
+//                return
+//            }
+//
+//
+//            do {
+//                print(String(decoding: data, as: UTF8.self))
+//                let room = try JSONDecoder().decode(Room.self, from: data)
+//                completion(room, nil)
+//            } catch {
+//                NSLog("Error decoding room: \(error)")
+//                completion(nil, error)
+//                return
+//            }
+//        }
+//
+//    .resume()
+//
+//    }
     
     
     
---
+++ TreasureHunt/TreasureHunt/Config.swift
@@ -0,0 +1,15 @@
+//
+//  Config.swift
+//  TreasureHunt
+//
+//  Created by Kobe McKee on 11/19/19.
+//  Copyright © 2019 Kobe McKee. All rights reserved.
+//
+
+import Foundation
+
+enum Config {
+    
+
+    
+}
